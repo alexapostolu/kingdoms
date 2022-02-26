2e15077168c9d7efe0e23cfbcd55f9fe990edbba
DynamicSquid <apostolu240@gmail.com>
2022-02-26 00:31:03 -0700

    main

---
+++ main.cpp
@@ -0,0 +1,47 @@
+#include <SDL.h>
+#include <stdio.h>
+
+#define SCREEN_WIDTH 900 
+#define SCREEN_HEIGHT 500
+
+int main(int argv, char** args)
+{
+    if(SDL_Init(SDL_INIT_VIDEO) < 0){
+        printf("Error: SDL failed to initialize\nSDL Error: '%s'\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Window *window = SDL_CreateWindow("SLD test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+    if(!window){
+        printf("Error: Failed to open window\nSDL Error: '%s'\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if(!renderer){
+        printf("Error: Failed to create renderer\nSDL Error: '%s'\n", SDL_GetError());
+        return 1;
+    }
+
+    bool running = true;
+    while(running){
+        SDL_Event event;
+        while(SDL_PollEvent(&event)){
+            switch(event.type){
+                case SDL_QUIT:
+                    running = false;
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 100, 255, 0, 255);
+        SDL_RenderClear(renderer);
+
+        SDL_RenderPresent(renderer);
+    }
+
+    return 0;
+}
