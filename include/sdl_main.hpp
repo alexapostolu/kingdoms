af2fd55929803a465ead340fb8df2fc5706edd06
DynamicSquid <60247838+DynamicSquid@users.noreply.github.com>
2022-03-14 14:59:19 -0600

    Update sdl_main.hpp

--- include/sdl_main.hpp
+++ include/sdl_main.hpp
@@ -26,19 +26,16 @@ using texture_ptr  = std::unique_ptr<SDL_Texture,  SDL_Deleter>;
 class SDL_Main
 {
 public:
-	void text(std::string_view text, TTF_Font const& font, TTF_Color const& colour)
+	void text(std::string_view text, TTF_Font const& font, TTF_Color const& colour) // size, alignment
 	{
-		SDL_Surface* text_surface = TTF_RenderText_Solid(font, text.c_str(), colour);
-		SDL_Texture* text_texture = SDL_CreateTextureFromSurface(renderer, text_surface);
+		sdl2::surface_ptr text_surface = TTF_RenderText_Solid(font, text.c_str(), colour);
+		sdl2::texture_ptr text_texture = SDL_CreateTextureFromSurface(renderer, text_surface);
 	
 		int text_w, text_h;
 		int s = TTF_SizeText(ttf_brygada, text_message.c_str(), &text_w, &text_h);
 		SDL_Rect text_rect{ (SCREEN_WIDTH - text_w) - 20, 20, text_w, text_h };
 
 		SDL_RenderCopy(renderer, text_texture, NULL, &text_rect);
-
-		SDL_FreeSurface(text_surface);
-		SDL_DestroyTexture(text_texture);
 	}
 
 private:
