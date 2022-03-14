b0d0237de1644e472604eda11bad526d29262a1d
DynamicSquid <60247838+DynamicSquid@users.noreply.github.com>
2022-03-14 14:42:17 -0600

    Update sdl_main.hpp

--- include/sdl_main.hpp
+++ include/sdl_main.hpp
@@ -5,6 +5,19 @@
 
 #include <memory>
 
+struct SDL_Deleter
+{
+	void operator()(SDL_Window*   ptr) { if (ptr) SDL_DestroyWindow(ptr); }
+	void operator()(SDL_Renderer* ptr) { if (ptr) SDL_DestroyRenderer(ptr); }
+	void operator()(SDL_Surface*  ptr) { if (ptr) SDL_FreeSurface(ptr); }
+	void operator()(SDL_Texture*  ptr) { if (ptr) SDL_DestroyTexture(ptr); }
+};
+
+using WindowPtr   = std::unique_ptr<SDL_Window,   SDL_Deleter>;
+using RendererPtr = std::unique_ptr<SDL_Renderer, SDL_Deleter>;
+using SurfacePtr  = std::unique_ptr<SDL_Surface,  SDL_Deleter>;
+using TexturePtr  = std::unique_ptr<SDL_Texture,  SDL_Deleter>;
+
 class SDL_Main
 {
 public:
