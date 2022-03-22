34cd114df13f575ae8b479ce1009ec9aa6bb2fcf
DynamicSquid <60247838+DynamicSquid@users.noreply.github.com>
2022-03-22 15:14:12 -0600

    Update base.hpp

--- include/base.hpp
+++ include/base.hpp
@@ -19,7 +19,7 @@ public:
 
 public:
     void display_resources();
-    void display_scene();
+	void display_scene();
     void display_shop();
 
     void handle_mouse_on_shop(int x, int y);
@@ -27,7 +27,7 @@ public:
 public:
 	int gold, wheat, wood, stone, gems;
 	int level, exp, troph;
-    bool edit_mode;
+	bool edit_mode;
 
     int const TILES_X, TILES_Y;
 
@@ -45,6 +45,6 @@ private:
     std::vector<std::vector<Tile>> tiles;
     std::vector<Person> farmers;
     std::vector<std::pair<std::string, std::vector<int> > > buildings;
-    // building being placed
+    // index of building being placed
     int place;
 };
