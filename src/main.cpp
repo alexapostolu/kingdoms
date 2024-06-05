d2d476535782fd9826a77f431cb96d6bc97a2721
alexapostolu <apostolu240@gmail.com>
2024-06-04 23:20:41 -0400

    new visual studio project, initial commit

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+﻿#include <SDL.h>
+#include <iostream>
+#include "kingdoms.h"
+
+int main(int argc, char* argv[])
+{
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    std::cout << "Hello Kingdoms!\n";
+    funct();
+
+    SDL_Quit();
+    return 0;
+}
